1a5599757df1b01f22860347295a1a01a4f369f4
bhatt <bhattprayas40@gmail.com>
2024-07-23 22:18:40 +0545

    y

--- src/Hotcold.java
+++ src/Hotcold.java
@@ -8,9 +8,31 @@ public class Hotcold {
 
         int num = 75;
 
-        if (int number > int num + 5 && int number < int num - 5){
-            System.out.println("Hot");
-        }
+//        if (int number > int num + 5 && int number < int num - 5){
+//            System.out.println("Hot");
+//        }
 
+//        if (int number > int num + 5 && int number < int num -5 || number )
+
+
+//        if (number > num - 5 && number < num +5){
+//            System.out.println("Hot");
+//        } else if (number > num - 10 && number < num +10){
+//            System.out.println("Cold");
+//        } else if (number > num - 20 && number < num +20){
+//            System.out.println("Extreme Cold");
+//        } else if (number > num - 2 && number < num +2 ){
+//            System.out.println("Extreme Hot");
+//        }
+
+        if (number > num - 2 && number < num + 2){
+            System.out.println("Extreme Hot");
+        } else if (number > num-5 && number < num + 5){
+            System.out.println("Hot");
+        } else if (number > num - 15 && number < num + 15){
+            System.out.println("Cold");
+        } else if (number > num - 30 && number < num + 30){
+            System.out.println("Extreme Cold");
+        }
     }
 }
